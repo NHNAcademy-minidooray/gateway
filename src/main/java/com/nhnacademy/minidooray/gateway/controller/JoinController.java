af8e7baae5968b4e38f789e70dfa0c78c0fbc413
muneunji <ansdmswl0722@gmail.com>
2023-06-07 14:50:39 +0900

    회원가입 controller

--- src/main/java/com/nhnacademy/minidooray/gateway/controller/JoinController.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/controller/JoinController.java
@@ -1,17 +1,15 @@
 package com.nhnacademy.minidooray.gateway.controller;
 
 import com.nhnacademy.minidooray.gateway.auth.AccountAdopter;
-import com.nhnacademy.minidooray.gateway.domain.Account;
 import com.nhnacademy.minidooray.gateway.domain.RegisterRequest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
-import java.util.Date;
 
 @Controller
 @RequestMapping("/join")
@@ -19,16 +17,16 @@ public class JoinController {
     private AccountAdopter accountAdopter;
     private BCryptPasswordEncoder bCrypt;
 
+    @GetMapping
+    public String join() {
+        return "join";
+    }
 
-    @PostMapping("/submit")
-    public String signup(@Valid @RequestBody RegisterRequest request){
+    @PostMapping
+    public String joinForm(@Valid @RequestBody RegisterRequest request){
         request.setPassword(bCrypt.encode(request.getPassword()));
         accountAdopter.createAccount(request);
-
-
-
-//        accountAdopter.
-       return "login";
+       return "redirect:/login";
     }
 
 }
